6915fb550d1d294d080c238d929b4c534685f598
GustavoCdnd <163040579+GustavoCdnd@users.noreply.github.com>
2025-11-05 22:31:57 -0400

    Create dados-atleta.js

---
+++ dados-atleta.js
@@ -0,0 +1,85 @@
+class Atleta {
+  constructor(nome, idade, peso, altura, notas) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+    this.notas = notas;
+  }
+
+  // Essa parte nao consegui desenvolver sozinho consultei e usei exemplos do chatgpt e reescrevi o codigo ate acertar
+  calculaCategoria() {
+    if (this.idade >= 9 && this.idade <= 11) {
+      this.categoria = "Infantil";
+    } else if (this.idade >= 12 && this.idade <= 13) {
+      this.categoria = "Juvenil";
+    } else if (this.idade >= 14 && this.idade <= 15) {
+      this.categoria = "Intermediário";
+    } else if (this.idade >= 16 && this.idade <= 30) {
+      this.categoria = "Adulto";
+    } else {
+      this.categoria = "Sem categoria";
+    }
+  }
+
+
+  calculaIMC() {
+    this.imc = this.peso / (this.altura * this.altura);
+  }
+
+ 
+  calculaMediaValida() {
+    let notasOrdenadas = this.notas.sort((a, b) => a - b);
+    let notasComputadas = notasOrdenadas.slice(1, 4);
+    let soma = 0;
+    notasComputadas.forEach((nota) => soma += nota);
+    this.mediaValida = soma / notasComputadas.length;
+  }
+
+
+  obtemNomeAtleta() {
+    return this.nome;
+  }
+
+  obtemIdadeAtleta() {
+    return this.idade;
+  }
+
+  obtemPesoAtleta() {
+    return this.peso;
+  }
+
+  obtemNotasAtleta() {
+    return this.notas.join(",");
+  }
+
+  obtemCategoria() {
+    return this.categoria;
+  }
+
+  obtemIMC() {
+    return this.imc;
+  }
+
+  obtemMediaValida() {
+    return this.mediaValida;
+  }
+}
+
+
+const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
+
+
+atleta.calculaCategoria();
+atleta.calculaIMC();
+atleta.calculaMediaValida();
+
+
+console.log(`Nome: ${atleta.obtemNomeAtleta()}`);
+console.log(`Idade: ${atleta.obtemIdadeAtleta()}`);
+console.log(`Peso: ${atleta.obtemPesoAtleta()}`);
+console.log(`Altura: ${atleta.altura}`);
+console.log(`Notas: ${atleta.obtemNotasAtleta()}`);
+console.log(`Categoria: ${atleta.obtemCategoria()}`);
+console.log(`IMC: ${atleta.obtemIMC()}`);
+console.log(`Média válida: ${atleta.obtemMediaValida()}`);
